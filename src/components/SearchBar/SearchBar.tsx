3499f9429fc31149dde63dd5713db22f19b38a2a
Epokuiso <epokuiso@gmail.com>
2022-04-10 14:35:48 +0100

    enter on input functionality add

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,21 +1,30 @@
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { getUserProfileInformation } from "../../api";
 import { UserInformationContext } from "../../context/UserInformationContext";
 import { Container } from "./styles";
 
 export const SearchBar = () =>
 {
-    const userInformationContext = useContext (UserInformationContext);
+    const [found, setFound] = useState (true);
+    let userInformationContext = useContext (UserInformationContext);
     
     const setUsername = (event: React.ChangeEvent<HTMLInputElement>) =>
     {
         userInformationContext.username = event.target.value;
     }
 
+    const pressedKey = async (key: React.KeyboardEvent<HTMLInputElement>) =>
+    {
+        key.code === 'Enter' ? await searchUsername(): await null;
+    }
+
     const searchUsername = async () =>
     {
-        console.log (await getUserProfileInformation (userInformationContext.username));
+        userInformationContext = await getUserProfileInformation (userInformationContext.username);
+        console.log (userInformationContext);
     }
+
+
 
     return (
         <Container>
@@ -23,6 +32,7 @@ export const SearchBar = () =>
             name='username' 
             placeholder='Enter a username' 
             onChange={ event => setUsername (event) }
+            onKeyUp={ event => pressedKey (event) }
             />
             <button onClick={ async () => await searchUsername() }>Search</button>
         </Container>
